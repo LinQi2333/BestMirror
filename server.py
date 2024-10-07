16b9b6f8c0d43a6dd114d008c78a82eb5d6ef69f
Rin <794922335@qq.com>
2024-10-08 01:27:38 +0800

    add DB path

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import requests
 import os
+import time
 from urllib import request
 
 localProxy = request.getproxies()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
 
 best_url = "https://storage.sekai.best/sekai-jp-assets/"
+bestDBurl = "http://sekai-world.github.io/sekai-master-db-diff/"
 test_path = "event_story/event_ashiato_2021/scenario_rip/event_39_08.asset"
 
 cache_dir = "./cache/"
+
+DB_name_list = ["areatalks", "cards", "events", "festivals", "greets", "mainStory", "setting", "specials"]
 
 # a = requests.get(best_url + test_path, headers=headers, proxies=localProxy).text
 # print(a)
@@ -18,8 +22,17 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class SimpleHTTPGetHandler(BaseHTTPRequestHandler):
  
     def do_GET(self):
-        url = self.path[1:] # Obtain paths without initial "/"
-        print(url)
+        file_name_with_ext = self.path.split('/')[-1]
+        file_name, file_ext = os.path.splitext(file_name_with_ext)
+
+        if file_name in DB_name_list:
+            self.handle_request(bestDBurl, file_name_with_ext)
+        else:
+            self.handle_request(best_url, self.path[1:])
+
+    def handle_request(self, base_url, url):
+        # url = self.path[1:] # Obtain paths without initial "/"
+        # print(url)
 
         cache_path = os.path.join(cache_dir, url)
         if os.path.isdir(cache_path):
@@ -27,10 +40,11 @@ class SimpleHTTPGetHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"Cannot process directory")
             return
         
+
         # Check if url exists
         if not os.path.exists(cache_path):
 
-            res = requests.get(best_url + url, headers = headers, proxies = localProxy)
+            res = requests.get(base_url + url, headers = headers, proxies = localProxy)
             if not res:
                 self.send_response(res.status_code)
                 return
