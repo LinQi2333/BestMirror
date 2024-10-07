fc20abb919ac010617c5a02e315ee2d6f30dbd2c
betairylia <betairya@gmail.com>
2024-10-08 04:24:54 +0900

    Header storage and transfering

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import os
 import time
 from urllib import request
 
+import pickle # TODO: Potential security problems?
+
 localProxy = request.getproxies()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
 
@@ -12,12 +14,20 @@ test_path = "event_story/event_ashiato_2021/scenario_rip/event_39_08.asset"
 
 cache_dir = "./cache/"
 
-cache_expiration_time = 600
+cache_expiration_time = 100
 
 # a = requests.get(best_url + test_path, headers=headers, proxies=localProxy).text
 # print(a)
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+ignore_headers = [
+    "Content-Encoding",
+    "Content-Length",
+    "expires",
+    "Cache-Control",
+    "Connection",
+]
  
 class SimpleHTTPGetHandler(BaseHTTPRequestHandler):
  
@@ -33,6 +43,8 @@ class SimpleHTTPGetHandler(BaseHTTPRequestHandler):
         # url = self.path[1:] # Obtain paths without initial "/"
         # print(url)
 
+        print("Origin - %s%s" % (base_url, url))
+
         cache_path = os.path.join(cache_dir, url)
         if os.path.isdir(cache_path):
             self.send_response(404)
@@ -47,20 +59,35 @@ class SimpleHTTPGetHandler(BaseHTTPRequestHandler):
                 self.send_response(res.status_code)
                 return
             
+            # breakpoint()
+
             dir_path = os.path.dirname(cache_path)
             os.makedirs(dir_path, exist_ok = True)
-            with open(cache_path, 'w') as f:
-                f.write(res.text)
+            with open(cache_path, 'wb') as f:
+                # f.write(res.text)
+                pickle.dump(res, f)
 
         # Path exists and not expired
         if os.path.exists(cache_path) and not self.is_cache_expired(cache_path):
-            with open(cache_path) as f:
-                contents = f.read()
+            with open(cache_path, "rb") as f:
+                # contents = f.read()
+                contents = pickle.load(f)
 
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            # self.send_header('Content-type', 'text/plain')
+
+            # breakpoint()
+
+            # Copy all headers
+            for header in contents.headers:
+                if header in ignore_headers: # Disable compression
+                    continue
+                # print("%s - %s" % (header, contents.headers[header]))
+                self.send_header(header, contents.headers[header])
+
             self.end_headers()
-            self.wfile.write(bytes(contents, encoding='utf-8'))
+            # self.wfile.write(bytes(contents, encoding='utf-8'))
+            self.wfile.write(contents.content)
             return
         else:# Path exists but expired
             if os.path.exists(cache_path):
